30b4c725749e8c1adfbd99545d1fae6c888bbb3c
Nimesh Akalanka <90953455+Mafia58@users.noreply.github.com>
2022-03-28 06:08:38 +0530

    Create configs.py

---
+++ configs.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config(object):
+    SESSION_NAME = Torrent-Search-Bot
+    API_ID = 8623612
+    API_HASH = 06ea2889c5517eb64017b032d667e29f
+    BOT_TOKEN = 2101626667:AAEzbUuBQtzLMfhb3N6vlinXc4HpDTkb1l4
+    MAX_INLINE_RESULTS = 50
